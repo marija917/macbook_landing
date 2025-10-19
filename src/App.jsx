05b1570ffa06e529ef9f4a1ab6286dfbe18ab7f1
marija917 <marijajonoska79@gmail.com>
2025-10-19 22:11:59 +0200

    Implement Highlights section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { ScrollTrigger } from "gsap/all";
 import Showcase from "./components/Showcase.jsx"
 import Performance from "./components/Performance.jsx"
 import Features from "./components/Features.jsx"
+import Highlights from "./components/Highlights.jsx"
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -18,6 +19,7 @@ const App = () => {
             <Showcase />
             <Performance />
             <Features />
+            <Highlights />
         </main>
     )
 }
