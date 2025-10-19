0f7f3fbd3c7560889c3817d9f4ada1bcfc78c6af
marija917 <marijajonoska79@gmail.com>
2025-10-19 21:58:46 +0200

    Implement Features section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import gsap from "gsap"
 import { ScrollTrigger } from "gsap/all";
 import Showcase from "./components/Showcase.jsx"
 import Performance from "./components/Performance.jsx"
+import Features from "./components/Features.jsx"
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -16,6 +17,7 @@ const App = () => {
             <ProductViewer />
             <Showcase />
             <Performance />
+            <Features />
         </main>
     )
 }
