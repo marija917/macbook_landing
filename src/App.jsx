118dc8e286b0931d71f1ff63eb51a09ccde6e006
marija917 <marijajonoska79@gmail.com>
2025-10-19 22:18:21 +0200

    Implement footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import NavBar from "./components/NavBar.jsx";
 import Hero from "./components/Hero.jsx";
 import ProductViewer from "./components/ProductViewer.jsx";
-import gsap from "gsap"
+import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/all";
-import Showcase from "./components/Showcase.jsx"
-import Performance from "./components/Performance.jsx"
-import Features from "./components/Features.jsx"
-import Highlights from "./components/Highlights.jsx"
+import Showcase from "./components/Showcase.jsx";
+import Performance from "./components/Performance.jsx";
+import Features from "./components/Features.jsx";
+import Highlights from "./components/Highlights.jsx";
+import Footer from "./components/Footer.jsx";
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -20,6 +21,7 @@ const App = () => {
             <Performance />
             <Features />
             <Highlights />
+            <Footer />
         </main>
     )
 }
